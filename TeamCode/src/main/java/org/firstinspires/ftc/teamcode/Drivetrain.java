d59f066a28b1ad699204b7bbf4abfd11cd3d64cb
Mokshith Addala <113805119+MokshithAddala28@users.noreply.github.com>
2024-10-01 09:26:08 -0300

    Format Changes
    Made changes to the format of the code and removed unnecessary files.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Drivetrain {
+    public static DcMotorEx leftFront;
+    public static DcMotorEx rightFront;
+    public static DcMotorEx leftRear;
+    public static DcMotorEx rightRear;
+    public static DcMotorEx armMovement;
+    public static DcMotorEx leftArmExtender;
+    public static DcMotorEx rightArmExtender;
+    public static Servo armWrist;
+    public static Servo claw;
+
+
+
+    }
